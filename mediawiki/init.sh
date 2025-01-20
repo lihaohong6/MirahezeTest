0e0662b2446b4513922b5aaa48b24a681de435ed
Peter Li <lihaohong6@gmail.com>
2025-01-20 02:20:43 -0500

    More comments. Make base path a variable.

--- mediawiki/init.sh
+++ mediawiki/init.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 
-cd /var/www/html && \
+MW_HOME=/var/www/html
+
+cd $MW_HOME && \
 php maintenance/run.php install.php --dbuser test --dbpass test "Miraheze Meta" Lih --pass MirahezeTest --dbserver mariadb && \
 php maintenance/run.php update.php --quick
 
 # install CheckUser
-cd /var/www/html
-cd extensions/
+cd $MW_HOME/extensions
 git clone -b REL1_42 https://gerrit.wikimedia.org/r/mediawiki/extensions/CheckUser
 composer update --no-dev
-cd ..
 
-# install other extensions else from composer.local.json
+# install other extensions from composer.local.json
+cd $MW_HOME
 composer update
 
+# append custom settings tweaks
 cat LocalSettings2.php >> LocalSettings.php
 
 # update all extensions
